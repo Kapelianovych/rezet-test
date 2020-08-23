67480055cfc200fffddfd5074f71e8f91e7b080f
Kapelianovych Yevhen <kapelianovych.y.v@gmail.com>
2020-08-23 16:40:30 +0300

    Remove onEnd listener of https.request due to absence of payload.

--- server/src/routes/resources.ts
+++ server/src/routes/resources.ts
@@ -6,10 +6,7 @@ const MOCKAPI_BASE_URL = 'https://5f3e43c013a9640016a6886d.mockapi.io/api';
 export default Router().get('/products/all', (req, res) => {
   https
     .request(`${MOCKAPI_BASE_URL}/products`, (response) => {
-      response
-        .setEncoding('utf8')
-        .on('data', (chunk) => res.send(chunk))
-        .on('end', (chunk: any) => res.send(chunk));
+      response.setEncoding('utf8').on('data', (chunk) => res.send(chunk));
     })
     .end();
 });
